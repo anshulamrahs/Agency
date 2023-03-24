3c337481b607ccae2de2857b5c04be9c2e76fa94
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-03-24 21:19:07 +0530

    topbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Anshul</h1>
+      <Topbar/>
     </div>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,19 @@ import './topbar.scss'
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <div className='topbar'>
+        <div className="left">
+            <span className='logo'>Wave.</span><span className='log'>x</span>
+        </div>
+        <div className="right">
+            <ul>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#home'>About</a></li>
+                <li><a href='#home'>Work</a></li>
+                <li><a href='#home'>Hire us!</a></li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
