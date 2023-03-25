bd800375aad181b760d1ad024c006f4d0a3d3c53
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-03-25 09:52:26 +0530

    home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Home from './components/home/Home';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <Home/>
     </div>
   );
 }
