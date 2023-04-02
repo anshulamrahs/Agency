c005f2c356a6489aff894e00b8e1c3e2a886ecae
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-02 11:26:39 +0530

    starter

--- src/components/asd.jsx
+++ src/components/asd.jsx
@@ -7,39 +7,3 @@ const asd = () => {
 }
 
 export default asd
-import React from 'react'
-
-const asd = () => {
-  return (
-    <div>asd</div>
-  )
-}
-
-export default asd
-import React from 'react'
-
-const asd = () => {
-  return (
-    <div>asd</div>
-  )
-}
-
-export default asd
-import React from 'react'
-
-const asd = () => {
-  return (
-    <div>asd</div>
-  )
-}
-
-export default asd
-import React from 'react'
-
-const asd = () => {
-  return (
-    <div>asd</div>
-  )
-}
-
-export default asd
