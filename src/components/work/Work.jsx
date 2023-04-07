63d3937feade8737353dbad040fff8c06282735f
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-07 10:10:36 +0530

    work component

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './work.scss'
 import WR from '../assets/wr.jpeg'
+import WR1 from '../assets/wr1.jpeg'
+import WR2 from '../assets/wr2.jpeg'
 
 const Work = () => {
   return (
@@ -19,8 +21,21 @@ const Work = () => {
         </div>
       </div>
       <div className="card card2">
+        <div className="left left2">
+          <img className='img img2' src={WR1} alt=""/>
+        </div>
+        <div className="right">
+          <h2>The Creek</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, cum.</p>
+          <div className="span">
+          <span>React</span><span>SCSS</span>
+          </div>
+          <button className="btn">View Demo</button>
+        </div>
+      </div>
+      <div className="card">
         <div className="left">
-          <img className='img img2' src={WR} alt=""/>
+          <img className='img' src={WR2} alt=""/>
         </div>
         <div className="right">
           <h2>The Creek</h2>
