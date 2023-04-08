fdb67b32326f592bc3b6612133280fc10ca93daa
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-08 10:09:41 +0530

    Services component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import About from './components/about/About';
 import Home from './components/home/Home';
+import Services from './components/services/Services';
 import Topbar from './components/topbar/Topbar';
 import Work from './components/work/Work';
 
@@ -12,6 +13,7 @@ function App() {
       <Home/>
       <About/>
       <Work/>
+      <Services/>
     </div>
   );
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './services.scss';
+import TR from '../assets/sym.png'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='services'>
+        <h1>What we Provide</h1>
+        <div className="cardHold">
+
+        <div className="card">
+            <img className='log' src={TR} alt=''/>
+            <p>UI/UX Design</p>
+        </div>
+        <div className="card">
+            <img className='log' src={TR} alt=''/>
+            <p>UI/UX Design</p>
+        </div>
+        <div className="card">
+            <img className='log' src={TR} alt=''/>
+            <p>UI/UX Design</p>
+        </div>
+        </div>
+        <div className="cardHold">
+
+        <div className="card">
+            <img className='log' src={TR} alt=''/>
+            <p>UI/UX Design</p>
+        </div>
+        <div className="card">
+            <img className='log' src={TR} alt=''/>
+            <p>UI/UX Design</p>
+        </div>
+        <div className="card">
+            <img className='log' src={TR} alt=''/>
+            <p>UI/UX Design</p>
+        </div>
+        </div>
+    </div>
   )
 }
 
