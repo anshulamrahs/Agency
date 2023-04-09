a22c83f608721dab7b9eed133927024a7575c06f
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-09 12:41:48 +0530

    Services component

---
+++ src/components/nakli.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const nakli = () => {
+  return (
+    <div>nakli</div>
+  )
+}
+
+export default nakli
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,33 +7,37 @@ const Services = () => {
     <div className='services'>
         <h1>What we Provide</h1>
         <div className="cardHold">
-
         <div className="card">
             <img className='log' src={TR} alt=''/>
-            <p>UI/UX Design</p>
+            <h3>UI/UX Design</h3>
+            <p>Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App</p>
         </div>
         <div className="card">
             <img className='log' src={TR} alt=''/>
-            <p>UI/UX Design</p>
+            <h3>Icon/Illustration</h3>
+            <p> Character Design, Icon Set, Illustration Guide, Illustration Set</p>
         </div>
         <div className="card">
             <img className='log' src={TR} alt=''/>
-            <p>UI/UX Design</p>
+            <h3>Branding</h3>
+            <p>Visual Identity, Stationary Kit, Marketing Materials</p>
         </div>
         </div>
         <div className="cardHold">
-
         <div className="card">
             <img className='log' src={TR} alt=''/>
-            <p>UI/UX Design</p>
+            <h3>Motion</h3>
+            <p>2d Animation, UI Motion</p>
         </div>
         <div className="card">
             <img className='log' src={TR} alt=''/>
-            <p>UI/UX Design</p>
+            <h3>Development</h3>
+            <p>HTML/CSS, JavaScript Animation, WordPress, Responsive Website</p>
         </div>
         <div className="card">
             <img className='log' src={TR} alt=''/>
-            <p>UI/UX Design</p>
+            <h3>Photography</h3>
+            <p>Travel Photography, Product Photography</p>
         </div>
         </div>
     </div>
