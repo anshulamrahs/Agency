a858f774c9967fb38eea6ae7f791875ac128d75b
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-17 18:45:13 +0530

    hero page revamp

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import './home.scss'
+import ROW from '../assets/tech.jpeg'
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 const Home = () => {
   return (
     <div className='home' id='home'>
       <div className="left">
-        <span className='head'>Hi,How can we Help.?</span>
-        <span className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis fuga minus odit exercitationem, assumenda nihil et dolorum aspernatur magnam necessitatibus explicabo ipsum repudiandae officia! Unde vero sint deserunt assumenda tempora.</span>
-        <div className="btn">
-          <button>Hire Us</button>
+      <div className="forSmall">
+        <div className="small">
+            <span>Building Stunning 
+              Projects, Creating attractive Brands</span>
+            </div>
+              <ArrowOutwardIcon className='icon'/>
+           
         </div>
+            <div className="forLa">
+              <span className='d'>Design.</span>
+              <span>Development.</span>
+              <span className='k'>Knowledge.</span>
+              </div>
       </div>
       <div className="right">
-        <img className='rgh' src="https://images.unsplash.com/photo-1678644897380-65e32b963bb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80" alt=''/>
+        <img className='rgh' src={ROW} alt=''/>
       </div>
     </div>
   )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,14 +8,14 @@ const Topbar = () => {
         <div className="left">
             <img src={Core} className='logo' alt=''/>
         </div>
-        <div className="right">
+            <div className="right">
             <ul>
-                <li><a href='#home'>Home</a></li>
+                <li><a href='#home'>Work</a></li>
                 <li><a href='#about'>About</a></li>
-                <li><a href='#work'>Work</a></li>
+                <li><a href='#work'>Experience</a></li>
                 <li><a href='#home'>Hire us!</a></li>
             </ul>
-        </div>
+            </div>
     </div>
   )
 }
