ed6bd491b9d86c49204e62543497d76a3536130e
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-19 12:28:41 +0530

    contact

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,6 +6,12 @@ const Services = () => {
   return (
     <div className='services'>
         <h1>Get in Touch</h1>
+        <form>
+          <input type='text' placeholder='Name'/>
+          <input type='email' placeholder='Email Address'/>
+          <textarea placeholder='Enquiry' cols='40'rows={6} ></textarea>
+          <button type='submit'>Send</button>
+        </form>
 
     </div>
   )
