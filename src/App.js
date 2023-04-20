9ac7cbedb7e209d3ce650f72c0a1698fa924fb62
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-20 13:58:12 +0530

    final commits

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import Services from './components/services/Services';
 import Topbar from './components/topbar/Topbar';
 import Work from './components/work/Work';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Work/>
       <Services/>
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './footer.scss'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AdUnitsIcon from '@mui/icons-material/AdUnits';
 
 export const Footer = () => {
   return (
     <div className='footer'>
-      <span>Copyright by waveyhigh media</span>
-      <span>Calls</span>
+      <div className="left">
+        <a href="">Products</a>
+        <a href="">About</a>
+        <a href="">Blog</a>
+        <a href="">Services</a>
+        <a href="">Jobs</a>
+      </div>
+      <span>Copyright© by CoreBrush Studios</span>
+      <div className="right">
+        <a>Instagram <InstagramIcon className="icon"/></a>
+        <a>LinkedIN<LinkedInIcon className="icon"/></a>
+        
+      
+      </div>
     </div>
   )
 }
+
+export default Footer
