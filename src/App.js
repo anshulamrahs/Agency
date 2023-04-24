5a7861cda865119ac0ec0606052cfd1d720a6231
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-24 15:58:55 +0530

    cursor redev

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import Services from './components/services/Services';
 import Topbar from './components/topbar/Topbar';
 import Work from './components/work/Work';
 import Footer from './components/footer/Footer';
+import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+      innerSize={12}
+      outerSize={10}
+      color='97, 237, 135'
+      outerAlpha={0.5}
+      innerScale={0.7}
+      outerScale={4}
+      />
       <Topbar/>
       <Home/>
       <About/>
